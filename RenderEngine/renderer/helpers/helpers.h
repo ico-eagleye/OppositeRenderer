c0a3e03d91515d884b954919a8b3db9f9dcd7122
Valdis Vilcans <voldemarz@gmail.com>
2014-06-18 20:30:04 +0200

    printfs still seemed not very consistent, hence made more flexible version of OPTIX_DEBUG_PRINT, left commented out

--- RenderEngine/renderer/helpers/helpers.h
+++ RenderEngine/renderer/helpers/helpers.h
@@ -6,80 +6,95 @@
 
 #pragma once
 #include "config.h"
-//#define PRINTF printf
-#define PRINTF rtPrintf // doesn't show up when output forwarded to a file
+
+// Printf issues
+//rtPrintf()
+//		- doesn't print spaces in a loop
+//		- consecutive calls sometimes cause format exception - http://celarek.at/2014/05/why-you-should-never-use-nvidia-optix/
+// printf():
+//		- not officially supported, sometimes causes crashes
+//		- doesn't print spaces in a loop based on depth value (SOMETIMES ?) if depth variable used in the print call before loop
+//
+// print functions most likely are not the cause it self, some people point to to memory
+// corruption issues. It does complicate debugging process, hence the mulptiple switches for the macro below
+
+
+#define STD_PRINTF 1
+#define PRINTF_SPACES 1		// printing multiple consecutive spaces seems ramdom - doesn't always work
+#define PRINTF_IDX 1		// printing multiple consecutive spaces seems ramdom - doesn't always work
+
+#ifdef STD_PRINTF
+#define PRINTF printf
+#else
+#define PRINTF rtPrintf
+#endif
 
 #if ENABLE_RENDER_DEBUG_OUTPUT
-
-#if PRINTF
 #define OPTIX_DEBUG_PRINT(depth, str, ...) \
-    if (launchIndex.x == 0 && launchIndex.y == 0) \
-    {  \
-    PRINTF("%d %d: ", launchIndex.x, launchIndex.y); \
-    for(int i = 0; i < depth; i++) { PRINTF(" "); } \
-    PRINTF(str, __VA_ARGS__); \
-    }
+	//if (launchIndex.x == 0 && launchIndex.y == 0) \
+	//{  \
+	//	if (PRINTF_IDX) \
+	//	{ \
+	//		if (STD_PRINTF) \
+	//			PRINTF("i %d, %d -", launchIndex.x, launchIndex.y); \
+	//		else \
+	//			PRINTF("i %d, %d - d %d -", launchIndex.x, launchIndex.y, depth); \
+	//	} \
+	//	if (PRINTF_SPACES) for(int i = 0; i < depth; i++) { PRINTF(" "); } \
+	//	PRINTF(str, __VA_ARGS__); \
+	//}
 
-#else
-
-// With rtPrintf use single output. Multiple consecutive can "Error in rtPrintf format string"
-// exception - http://celarek.at/2014/05/why-you-should-never-use-nvidia-optix/
-// Some say it's indication of memory corruption somewhere.. investigating.
+// original
 #define OPTIX_DEBUG_PRINT(depth, str, ...) \
-    if (launchIndex.x == 0 && launchIndex.y == 0) \
-    {  \
-    PRINTF(str, __VA_ARGS__); \
-    }
-#endif
-//#define OPTIX_DEBUG_PRINT(depth, str, ...) \
-//    if (launchIndex.x == 0 && launchIndex.y == 0) \
-//    {  \
-//        PRINTF("i %d, %d - d %d - ", launchIndex.x, launchIndex.y, depth); \
-//        for(int i = 0; i < depth; i++) { PRINTF(" "); } \
-//        PRINTF(str, __VA_ARGS__); \
-//    }
-//#endif
-
+	if (launchIndex.x == 0 && launchIndex.y == 0) \
+	{  \
+	PRINTF("%d %d: ", launchIndex.x, launchIndex.y); \
+	for(int i = 0; i < depth; i++) { PRINTF(" "); } \
+	PRINTF(str, __VA_ARGS__); \
+	}
 #else
 #define OPTIX_DEBUG_PRINT(depth, str, ...) // nothing
 #endif
 
+
+
+
 // Create ONB from normalized normal (code: Physically Based Rendering, Pharr & Humphreys pg. 63)
 static  __device__ __inline__ void createCoordinateSystem( const optix::float3& N, optix::float3& U, optix::float3& V/*, optix::float3& W*/ )
 {
-    using namespace optix;
+	using namespace optix;
 
-    if(fabs(N.x) > fabs(N.y))
-    {
-        float invLength = 1.f/sqrtf(N.x*N.x + N.z*N.z);
-        U = make_float3(-N.z*invLength, 0.f, N.x*invLength);
-    }
-    else
-    {
-        float invLength = 1.f/sqrtf(N.y*N.y + N.z*N.z);
-        U = make_float3(0.f, N.z*invLength, -N.y*invLength);
-    }
-    V = cross(N, U);
+	if(fabs(N.x) > fabs(N.y))
+	{
+		float invLength = 1.f/sqrtf(N.x*N.x + N.z*N.z);
+		U = make_float3(-N.z*invLength, 0.f, N.x*invLength);
+	}
+	else
+	{
+		float invLength = 1.f/sqrtf(N.y*N.y + N.z*N.z);
+		U = make_float3(0.f, N.z*invLength, -N.y*invLength);
+	}
+	V = cross(N, U);
 }
 
 static __device__ __host__ __inline__ float maxf(float a, float b)
 {
-    return a > b ? a : b;
+	return a > b ? a : b;
 }
 
 // Returns true if ray direction points in the opposite direction 
 // as the normal, where the normal points outwards from the face
 static __device__ __host__ __inline__ bool hitFromOutside(const optix::float3 & rayDirection, const optix::float3 & normal)
 {
-    return (optix::dot(normal, rayDirection) < 0);
+	return (optix::dot(normal, rayDirection) < 0);
 }
 
 static __device__ __inline__ int intmin(int a, int b)
 {
-    return a < b ? a : b;
+	return a < b ? a : b;
 }
 
 static __device__ __inline__ float favgf(const optix::float3 & v )
 {
-    return (v.x+v.y+v.z)*0.3333333333f;
+	return (v.x+v.y+v.z)*0.3333333333f;
 }
