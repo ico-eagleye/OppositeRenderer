ebc26a7458c3d1c5c04422866a45ea6bd44bfb12
Valdis Vilcans <voldemarz@gmail.com>
2014-06-19 23:31:49 +0200

    Note about use of PhotonPRD.weight

--- RenderEngine/renderer/ppm/PhotonPRD.h
+++ RenderEngine/renderer/ppm/PhotonPRD.h
@@ -9,7 +9,7 @@
 struct PhotonPRD
 {
     optix::float3 power;
-    float weight;
+    float weight;					// vmarz: initially 1, scaled by fmax(Kd) at every hit, used to stop tracing when small
     optix::uint pm_index;			// vmarz: index of the first photon stored by a given light path/thread
     optix::uint numStoredPhotons;
     optix::uint depth;
