767873130345f7e0fb5ae7a51e58015ffea228b4
Valdis Vilcans <voldemarz@gmail.com>
2014-07-10 04:43:19 +0200

    Fix pathLen type

--- RenderEngine/renderer/vcm/LightVertex.h
+++ RenderEngine/renderer/vcm/LightVertex.h
@@ -3,17 +3,18 @@
 #pragma once
 //#include <optix_world.h>
 #include "material/BSDF.h"
+#include "optix.h"
 
 struct LightVertex
 {
-    float3  hitPoint;
-    float3  throughput;
+    optix::float3    hitPoint;
+    optix::float3   throughput;
     VcmBSDF bsdf;
-    uint2   launchIndex;    // for debug TODO remove
-    float   pathLen;
-    float   dVCM;
-    float   dVC;
-    float   dVM;
+    optix::uint2    launchIndex;    // for debug TODO remove
+    optix::uint     pathLen;
+    float           dVCM;
+    float           dVC;
+    float           dVM;
 //#if VCM_UNIFORM_VERTEX_SAMPLING
 //    float   dVC_unif_vert;
 //#endif
